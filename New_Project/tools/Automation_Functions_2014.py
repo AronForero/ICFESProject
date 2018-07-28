1e94d5d35c42785ecec3421f4fda8a40c1ef1c4e
Aron Forero <foreroo965@gmail.com>
2018-07-28 11:50:22 -0500

    columns of the other files checked, now just have to do the segmentation and experimentation, plot everything and drink a lot

--- New_Project/tools/Automation_Functions_2014.py
+++ New_Project/tools/Automation_Functions_2014.py
@@ -1,9 +1,4 @@
-def MAD(Y, YP): #MAE - MEAN ABSOLUTE ERROR FOR ME
-    """Returns the MEAN ABSOLUTE ERROR between the prediction and the real value"""
-    m = np.mean(abs(Y-YP))
-    return m
-
-def Prepare_data(data, c):
+def Prepare_data_2014(data, c):
     """Returns the data ready to be used in a prediction.
     
     -data: full dataset
@@ -15,43 +10,45 @@ def Prepare_data(data, c):
         -DT_Data: Old Predictive variables transformed by PCA of 33 components. This will be used for the Decision Tree Object
         -Y: Dataframe with the chosen target and the others"""
     data = data.sort_values(by=c) 
-    y_list = ['PUNT_BIOLOGIA', 'PUNT_MATEMATICAS', 'PUNT_FILOSOFIA', 'PUNT_FISICA', 'PUNT_INTERDISCIPLINAR', 'PUNT_QUIMICA', 
-              'PUNT_LENGUAJE', 'PUNT_GEOGRAFIA', 'PUNT_HISTORIA', 'PUNT_IDIOMA', 'PUNT_C_SOCIALES', 'PUNT_PROFUNDIZACION',
-              'ESTU_PUESTO', 'PUNT_INGLES', 'PUNT_COMP_FLEXIBLE']
+    y_list = ['DECIL_LECTURA_CRITICA', 'PUNT_LECTURA_CRITICA', 'DECIL_MATEMATICAS', 'PUNT_MATEMATICAS', 'DECIL_C_NATURALES',
+              'PUNT_C_NATURALES', 'DECIL_SOCIALES_CIUDADANAS', 'PUNT_SOCIALES_CIUDADANAS', 'DECIL_INGLES', 'DESEMP_INGLES',
+              'PUNT_INGLES', 'DECIL_RAZONA_CUANT', 'PUNT_RAZONA_CUANT', 'DECIL_COMP_CIUDADANA', 'PUNT_COMP_CIUDADANA', 
+              'PUNT_GLOBAL', 'ESTU_PUESTO']
+    new_y_list = ['PUNT_LECTURA_CRITICA', 'PUNT_MATEMATICAS', 'PUNT_C_NATURALES', 'PUNT_SOCIALES_CIUDADANAS', 'PUNT_INGLES',
+             'PUNT_RAZONA_CUANT', 'PUNT_COMP_CIUDADANA', 'PUNT_GLOBAL']
     X_list = data.columns.difference(y_list)
     
-    Y = data.filter(items = y_list)
+    Y = data.filter(items = new_y_list)
     X_data = data.filter(items = X_list)
     
-    if len(X_data.columns) < 35:
-        LR_Data = 0
-    else:
-        pca_LR = PCA(n_components=35);
-        LR_Data = pca_LR.fit_transform(X_data);
-        LR_Data = pd.DataFrame(LR_Data)
+    poly = PolynomialFeatures(degree=2)
+    LR_Data = poly.fit_transform(X_data);
+    LR_Data = pd.DataFrame(LR_Data)
     
-    if len(X_data.columns) < 33:
-        DT_Data = 0
-    else:
-        pca_DT = PCA(n_components=33);
-        DT_Data = pca_DT.fit_transform(X_data);
-        DT_Data = pd.DataFrame(DT_Data)
-    
-    ward = Prepare_RFR_data(data)
+    ward = Prepare_RFR_data_2014(data)
     if ward == 1:
-        New_x_list = ['ESTU_GENERO', 'ESTU_ACT_PROX_ANNO', 'COD_INTERDISCIPLINAR', 'COLE_CARACTER', 'ESTU_RESIDE_DEPTO',
-                      'FAMI_APORTANTES', 'FAMI_NUM_HERMANOS_EDUSUPERIOR', 'COLE_JORNADA', 'FAMI_OCUPA_MADRE',
-                      'ESTU_CARRDESEADA_RAZON', 'FAMI_PERSONAS_HOGAR', 'ESTU_RAZONINSTITUTO', 'FAMI_OCUPA_PADRE',
-                      'FAMI_EDUCA_PADRE', 'FAMI_NUM_HERMANOS', 'FAMI_EDUCA_MADRE', 'COLE_VALOR_PENSION', 'ESTU_RESIDE_MCPIO',
-                      'ESTU_NACIMIENTO_MES', 'ESTU_IES_COD_DESEADA', 'ESTU_NACIMIENTO_DIA', 'ESTU_NACIMIENTO_ANNO',
-                      'ESTU_CARRDESEADA_COD', 'COLE_COD_ICFES', 'FAMI_INGRESO_FMILIAR_MENSUAL']
-        X2 = data.filter(items=New_x_list)**2
+        X_list = ['COLE_AREA_UBICACION', 'COLE_BILINGUE', 'COLE_CALENDARIO', 'COLE_CARACTER', 'COLE_COD_ICFES', 'COLE_GENERO',
+                  'COLE_JORNADA', 'COLE_NATURALEZA', 'ESTU_COD_OTRO_PAIS_PLANTEL', 'ESTU_COD_PLANTEL', 'ESTU_COD_RESIDE_DEPTO',
+                  'ESTU_DEPTO_PRESENTACION', 'ESTU_EDAD', 'ESTU_ESTUDIANTE', 'ESTU_ETNIA', 'ESTU_GENERO', 
+                  'ESTU_IES_COD_DESEADA', 'ESTU_IES_MPIO_DESEADA', 'ESTU_MCPIO_PRESENTACION', 'ESTU_NACIMIENTO_DIA',
+                  'ESTU_NACIMIENTO_MES', 'ESTU_PAIS_RESIDE', 'ESTU_PRESENTO_ANTECEDENTES', 'ESTU_PRESENTO_EXPECTATIVAS',
+                  'ESTU_PRIVADO_LIBERTAD', 'ESTU_TIPO_CARRERA_DESEADA', 'ESTU_TRABAJA', 'ESTU_VECES_ESTADO',
+                  'ESTU_VECES_ESTADO_ESTUDIANTIL', 'ESTU_ZONA_RESIDE', 'FAMI_AUTOMOVIL', 'FAMI_CELULAR', 'FAMI_CUARTOS_HOGAR',
+                  'FAMI_DVD', 'FAMI_EDUCA_PADRE', 'FAMI_HORNO', 'FAMI_INGRESO_FMILIAR_MENSUAL', 'FAMI_INTERNET', 
+                  'FAMI_LAVADORA','FAMI_MICROONDAS', 'FAMI_NEVERA', 'FAMI_NIVEL_SISBEN', 'FAMI_OCUPA_MADRE', 
+                  'FAMI_OCUPA_PADRE','FAMI_PERSONAS_HOGAR', 'FAMI_PISOSHOGAR', 'FAMI_SERVICIO_TELEVISION', 
+                  'FAMI_TELEFONO_FIJO']
+        X = data.filter(items=New_x_list)
+        DT_Data = X
+        RF_Data = X
     else:
-        X2 = 0
+        X = 0
+        DT_Data = X
+        RF_Data = X
     
-    return(X2, LR_Data, DT_Data, Y)
+    return(LR_Data, DT_Data, RF_Data, Y)
 
-def Prepare_RFR_data(data):
+def Prepare_RFR_data_2014(data):
     """This function checks if all of the columns contained in the New X data are in the dataframe passed.
     
     Returns:
@@ -59,26 +56,32 @@ def Prepare_RFR_data(data):
         0 if aren't 
     """
     ward = 0
-    New_x_list = ['ESTU_GENERO', 'ESTU_ACT_PROX_ANNO', 'COD_INTERDISCIPLINAR', 'COLE_CARACTER', 'ESTU_RESIDE_DEPTO',
-                  'FAMI_APORTANTES', 'FAMI_NUM_HERMANOS_EDUSUPERIOR', 'COLE_JORNADA', 'FAMI_OCUPA_MADRE',
-                  'ESTU_CARRDESEADA_RAZON', 'FAMI_PERSONAS_HOGAR', 'ESTU_RAZONINSTITUTO', 'FAMI_OCUPA_PADRE',
-                  'FAMI_EDUCA_PADRE', 'FAMI_NUM_HERMANOS', 'FAMI_EDUCA_MADRE', 'COLE_VALOR_PENSION', 'ESTU_RESIDE_MCPIO',
-                  'ESTU_NACIMIENTO_MES', 'ESTU_IES_COD_DESEADA', 'ESTU_NACIMIENTO_DIA', 'ESTU_NACIMIENTO_ANNO',
-                  'ESTU_CARRDESEADA_COD', 'COLE_COD_ICFES', 'FAMI_INGRESO_FMILIAR_MENSUAL']
-    y_list = ['PUNT_BIOLOGIA', 'PUNT_MATEMATICAS', 'PUNT_FILOSOFIA', 'PUNT_FISICA', 'PUNT_INTERDISCIPLINAR', 'PUNT_QUIMICA', 
-              'PUNT_LENGUAJE', 'PUNT_GEOGRAFIA', 'PUNT_HISTORIA', 'PUNT_IDIOMA', 'PUNT_C_SOCIALES', 'PUNT_PROFUNDIZACION',
-              'ESTU_PUESTO', 'PUNT_INGLES', 'PUNT_COMP_FLEXIBLE']
-    for i in New_x_list:
-        if i in data.columns.difference(y_list):
+    X_list = ['COLE_AREA_UBICACION', 'COLE_BILINGUE', 'COLE_CALENDARIO', 'COLE_CARACTER', 'COLE_COD_ICFES', 'COLE_GENERO',
+              'COLE_JORNADA', 'COLE_NATURALEZA', 'ESTU_COD_OTRO_PAIS_PLANTEL', 'ESTU_COD_PLANTEL', 'ESTU_COD_RESIDE_DEPTO',
+              'ESTU_DEPTO_PRESENTACION', 'ESTU_EDAD', 'ESTU_ESTUDIANTE', 'ESTU_ETNIA', 'ESTU_GENERO', 
+              'ESTU_IES_COD_DESEADA', 'ESTU_IES_MPIO_DESEADA', 'ESTU_MCPIO_PRESENTACION', 'ESTU_NACIMIENTO_DIA',
+              'ESTU_NACIMIENTO_MES', 'ESTU_PAIS_RESIDE', 'ESTU_PRESENTO_ANTECEDENTES', 'ESTU_PRESENTO_EXPECTATIVAS',
+              'ESTU_PRIVADO_LIBERTAD', 'ESTU_TIPO_CARRERA_DESEADA', 'ESTU_TRABAJA', 'ESTU_VECES_ESTADO',
+              'ESTU_VECES_ESTADO_ESTUDIANTIL', 'ESTU_ZONA_RESIDE', 'FAMI_AUTOMOVIL', 'FAMI_CELULAR', 'FAMI_CUARTOS_HOGAR',
+              'FAMI_DVD', 'FAMI_EDUCA_PADRE', 'FAMI_HORNO', 'FAMI_INGRESO_FMILIAR_MENSUAL', 'FAMI_INTERNET', 'FAMI_LAVADORA',
+              'FAMI_MICROONDAS', 'FAMI_NEVERA', 'FAMI_NIVEL_SISBEN', 'FAMI_OCUPA_MADRE', 'FAMI_OCUPA_PADRE',
+              'FAMI_PERSONAS_HOGAR', 'FAMI_PISOSHOGAR', 'FAMI_SERVICIO_TELEVISION', 'FAMI_TELEFONO_FIJO']
+    y_list = ['DECIL_LECTURA_CRITICA', 'PUNT_LECTURA_CRITICA', 'DECIL_MATEMATICAS', 'PUNT_MATEMATICAS', 'DECIL_C_NATURALES',
+              'PUNT_C_NATURALES', 'DECIL_SOCIALES_CIUDADANAS', 'PUNT_SOCIALES_CIUDADANAS', 'DECIL_INGLES', 'DESEMP_INGLES',
+              'PUNT_INGLES', 'DECIL_RAZONA_CUANT', 'PUNT_RAZONA_CUANT', 'DECIL_COMP_CIUDADANA', 'PUNT_COMP_CIUDADANA', 
+              'PUNT_GLOBAL', 'ESTU_PUESTO']
+    diff = data.columns.difference(y_list)
+    for i in X_list:
+        if i in diff:
             ward+=1
     
-    if ward == 25:
+    if ward == 48:
         return(1)
     else:
         return(0)
 
 
-def Get_Scores(X2, LR_Data, DT_Data, Y, c):
+def Get_Scores_2014(RF_Data, LR_Data, DT_Data, Y, c):
     """Train the three models (LinearRegression, DecisionTreeRegressor, RandomForestRegressor), and 
     test them. All of this with a cross_val_score object
     
@@ -102,44 +105,36 @@ def Get_Scores(X2, LR_Data, DT_Data, Y, c):
         DT_scores = [-1,-1,-1,-1,-1]
     else:
         cv = ShuffleSplit(n = DT_Data.shape[0], n_iter=5, test_size=0.2)
-        DT = DecisionTreeRegressor(max_depth=6)
+        DT = DecisionTreeRegressor(max_depth=10)
         DT_scores = -cross_val_score(DT, DT_Data, Y[c], scoring='mean_absolute_error', cv = cv, n_jobs=4)
     
-    if type(X2) != pd.core.frame.DataFrame:
+    if type(RF_Data) != pd.core.frame.DataFrame:
         RF_scores = [-1,-1,-1,-1,-1]
     else:
-        RF = RandomForestRegressor(max_depth=10, n_jobs=4)
-        RF_scores = -cross_val_score(RF, X2, Y[c], scoring='mean_absolute_error', cv = cv, n_jobs=4)
+        cv = ShuffleSplit(n = RF_Data.shape[0], n_iter=5, test_size=0.2)
+        RF = RandomForestRegressor(max_depth=11, n_jobs=4)
+        RF_scores = -cross_val_score(RF, RF_Data, Y[c], scoring='mean_absolute_error', cv = cv, n_jobs=4)
         
     return(LR_scores, DT_scores, RF_scores)
 
-def Show_Score(LR_scores, DT_scores, RF_scores, i):
-    """Shows the Mean Score and the Standard Deviation of the given scores."""
-    print('Scores for the column:', i)
-    print('Linear Regression:      ','Mean Score', np.mean(LR_scores), 'STD Scores', np.std(LR_scores))
-    print('Decision Tree Regressor:', 'Mean Score', np.mean(DT_scores), 'STD Scores', np.std(DT_scores))
-    print('Random Forest:          ', 'Mean Score', np.mean(RF_scores), 'STD Scores', np.std(RF_scores))
-    print()
-    
-def Predict_all_Subjects(dataset):
+def Predict_all_Subjects_2014(dataset):
     """Uses the Automation functions to train and test models for each Subject in the given Dataset.
     dataset: Dataset to be used for the models
     
     Returns:
         -One Dataset with all the Scores indexed by subject"""
     
-    y_list = ['PUNT_BIOLOGIA', 'PUNT_MATEMATICAS', 'PUNT_FILOSOFIA', 'PUNT_FISICA', 'PUNT_INTERDISCIPLINAR', 'PUNT_QUIMICA', 
-              'PUNT_LENGUAJE', 'PUNT_GEOGRAFIA', 'PUNT_HISTORIA', 'PUNT_IDIOMA', 'PUNT_C_SOCIALES', 'PUNT_PROFUNDIZACION',
-              'ESTU_PUESTO', 'PUNT_INGLES', 'PUNT_COMP_FLEXIBLE']
-    Y_slice = dataset.filter(items=y_list)
+    new_y_list = ['PUNT_LECTURA_CRITICA', 'PUNT_MATEMATICAS', 'PUNT_C_NATURALES', 'PUNT_SOCIALES_CIUDADANAS', 'PUNT_INGLES',
+                  'PUNT_RAZONA_CUANT', 'PUNT_COMP_CIUDADANA', 'PUNT_GLOBAL']
+    Y_slice = dataset.filter(items=new_y_list)
     
     LR_List = []
     DT_List = []
     RF_List = []
     idx_List = []
     for i in Y_slice.columns:
-        X2, LR_Data, DT_Data, Y = Prepare_data(dataset, i)
-        LR_scores, DT_scores, RF_scores = Get_Scores(X2, LR_Data, DT_Data, Y, i)
+        LR_Data, DT_Data, RF_Data, Y = Prepare_data_2014(dataset, i)
+        LR_scores, DT_scores, RF_scores = Get_Scores_2014(RF_Data, LR_Data, DT_Data, Y, i)
         Show_Score(LR_scores, DT_scores, RF_scores, i)
         idx_List.append(i)
         LR_List.append(np.mean(LR_scores))
@@ -152,7 +147,7 @@ def Predict_all_Subjects(dataset):
 #############################################################################################
 #############################################################################################
 
-def Test_with_2sets(dataset1, dataset2, c):
+def Test_with_2sets_2014(dataset1, dataset2, c):
     """Train three models with the first Dataset, and Test the models with the second Dataset,
     -Shows the Mean Absolute Error of each model.
     
@@ -164,8 +159,8 @@ def Test_with_2sets(dataset1, dataset2, c):
         -The trained Models
         -The Score of each Model"""
     
-    X2, LR_Data, DT_Data, Y = Prepare_data(dataset1, c)
-    X2_2, LR_Data_2, DT_Data_2, Y_2 = Prepare_data(dataset2, c)
+    LR_Data, DT_Data, RF_Data, Y = Prepare_data_2014(dataset1, c)
+    LR_Data_2, DT_Data_2, RF_Data_2, Y_2 = Prepare_data_2014(dataset2, c)
     
     ##################TRAINING AND TESTING ######################
     print('Scores for the Columns/Subject:', c)
@@ -190,21 +185,21 @@ def Test_with_2sets(dataset1, dataset2, c):
     else:
         DT = DecisionTreeRegressor(max_depth=7);
         DT.fit(DT_Data, Y[c]);
-        if type(LR_Data_2) != pd.core.frame.DataFrame:
+        if type(DT_Data_2) != pd.core.frame.DataFrame:
             print('DATA ERROR... Decision Tree: Se pudo entrenar pero no validar.')
             DT_Score = -1
         else:
             DT_Score = MAD(Y_2[c], DT.predict(DT_Data_2))
             print('Score DT:', DT_Score)
     
-    if type(X2) != pd.core.frame.DataFrame:
+    if type(RF_Data) != pd.core.frame.DataFrame:
         RF = 0
         RF_Score = -1
         print('DATA ERROR... Random Forest: No se pudo entrenar el modelo.')
     else:
         RF = RandomForestRegressor(max_depth=10, n_jobs=4);
-        RF.fit(X2, Y[c]);
-        if type(LR_Data_2) != pd.core.frame.DataFrame:
+        RF.fit(RF_Data, Y[c]);
+        if type(RF_Data_2) != pd.core.frame.DataFrame:
             print('DATA ERROR... Random Forest: Se pudo entrenar pero no validar.')
             RF_Score = -1
         else:
@@ -215,7 +210,7 @@ def Test_with_2sets(dataset1, dataset2, c):
     
     return(LR, LR_Score, DT, DT_Score, RF, RF_Score)
 
-def Test_all_Subjects(dataset1, dataset2):
+def Test_all_Subjects_2014(dataset1, dataset2):
     """Uses the Test_with_2sets function to do the corresponding predictions for each Subject.
     
     Shows the Prediction of the Subject for each model. 
@@ -226,11 +221,10 @@ def Test_all_Subjects(dataset1, dataset2):
     Returns:
         -One DataFrame with all the Scores for each Subject."""
     
-    y_list = ['PUNT_BIOLOGIA', 'PUNT_MATEMATICAS', 'PUNT_FILOSOFIA', 'PUNT_FISICA', 'PUNT_INTERDISCIPLINAR', 'PUNT_QUIMICA', 
-              'PUNT_LENGUAJE', 'PUNT_GEOGRAFIA', 'PUNT_HISTORIA', 'PUNT_IDIOMA', 'PUNT_C_SOCIALES', 'PUNT_PROFUNDIZACION',
-              'ESTU_PUESTO', 'PUNT_INGLES', 'PUNT_COMP_FLEXIBLE']
-    d1_y_slice = set(dataset1.filter(items=y_list))
-    d2_y_slice = set(dataset2.filter(items=y_list))
+    new_y_list = ['PUNT_LECTURA_CRITICA', 'PUNT_MATEMATICAS', 'PUNT_C_NATURALES', 'PUNT_SOCIALES_CIUDADANAS', 'PUNT_INGLES',
+                  'PUNT_RAZONA_CUANT', 'PUNT_COMP_CIUDADANA', 'PUNT_GLOBAL']
+    d1_y_slice = set(dataset1.filter(items=new_y_list))
+    d2_y_slice = set(dataset2.filter(items=new_y_list))
     
     y_slice = d1_y_slice & d2_y_slice
     
@@ -239,7 +233,7 @@ def Test_all_Subjects(dataset1, dataset2):
     RF_List = []
     idx_List = []
     for i in y_slice:
-        LR, LR_Score, DT, DT_Score, RF, RF_Score = Test_with_2sets(dataset1, dataset2, i)
+        LR, LR_Score, DT, DT_Score, RF, RF_Score = Test_with_2sets_2014(dataset1, dataset2, i)
         idx_List.append(i)
         LR_List.append(LR_Score)
         DT_List.append(DT_Score)
